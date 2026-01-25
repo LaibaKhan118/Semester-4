4d3c0111861a7574e026fd303486ac0fb1105629
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2026-01-25 17:19:27 +0500

    Update Q1.py

--- AI/Lab/Lab1/Q1.py
+++ AI/Lab/Lab1/Q1.py
@@ -1,11 +1,11 @@
 name = input("Enter student name: ")
 marks = float(input("Enter student marks: "))
 
-if(marks >= 80):
+if marks >= 80:
     grade = 'A'
-elif(marks >= 70):
+elif marks >= 70:
     grade='B'
-elif(marks >= 50):
+elif marks >= 50:
     grade='C'
 else:
     grade='F'
